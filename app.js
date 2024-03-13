d22d63a10a112abe1abc8046a4e86ec6cc79b483
DJ Patrias <122762644+Patrias702@users.noreply.github.com>
2024-03-12 20:50:15 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// import http from "http"
+// here written as CommonJS
+const { appendFile } = require("fs");
+const http = require("http");
+
+http.createServer((request, response) => {
+  response.writeHead(200, { "content-type": "text/html" });
+  response.write("Hello World!");
+  response.end();
+}).listen(5000, () => {
+  console.log("Server listening at http://localhost:5000...");
+});
